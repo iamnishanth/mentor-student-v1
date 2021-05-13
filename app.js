93481e6bea636b711b667c720f6d1be9fd49d79b
iamnishanth <nishanthvenk68@gmail.com>
2021-05-13 22:36:51 +0530

    make it better

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.port || 5000;
 const db = require("./config/db");
 const cors = require("cors");
 
